d8bcfcb4916d0208a5889ca0e20a7db377d1964b
Grigory-RobinGood <grigoriy1986biz@mail.ru>
2024-07-30 15:25:40 +0300

    Update module2_hard.py

--- module2_hard.py
+++ module2_hard.py
@@ -1,21 +1,18 @@
 def generate_password(number):
     result = ""
 
-    for i in range(1, 21):  # Нахожу все возможные числа от 1 до 20
-        for j in range(i, 21):
-            pair_sum = i + j
+    for i in range(1, 21):              # Нахожу все возможные числа от 1 до 20
+        for j in range(i + 1, 21):
+            pair_sum = i + j            # Нахожу сумму чисел
             if number % pair_sum == 0:  # Проверяю кратность
-                if i == j:  # Формирую строку для пароля
-                    result += f"{i}{j}"
-                else:
-                    result += f"{i}{j}"
+                result += f"{i}{j}"     # Вывожу результат в строку
 
     return result
 
 
-num = int(input("Введите число от 3 до 20: "))
-if 3 <= num <= 20:
-    password = generate_password(num)
-    print(f"Пароль для числа {num}: {password}")
+num = int(input("Введите число от 3 до 20: "))      # Ввводим число
+if 3 <= num <= 20:                                  # Проверяем диапазон чисел
+    password = generate_password(num)               # Запускаем функцию
+    print(f"Пароль для числа {num}: {password}")    # Выводим результат
 else:
-    print("Число должно быть от 3 до 20.")
+    print("Число должно быть от 3 до 20.")          # Ошибка при выходе за диапазон
